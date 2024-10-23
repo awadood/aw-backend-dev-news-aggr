17e92d7b2c6c74585513bf4c81b56dba9c83ae34
Abdul Wadood <awadoodraj@gmail.com>
2024-10-23 17:38:41 +0200

    routes added for user preferences
    routes added for user preferences

--- app/Constants/RouteNames.php
+++ app/Constants/RouteNames.php
@@ -5,7 +5,14 @@ namespace App\Constants;
 class RouteNames
 {
     const AUTH_REGISTER = 'auth.register';
+
     const AUTH_LOGIN = 'auth.login';
+
     const AUTH_LOGOUT = 'auth.logout';
+
     const PASSWORD_RESET = 'password.reset';
+
+    const PREF_SHOW = 'preferences.show';
+
+    const PREF_STORE = 'preferences.store';
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Constants\RouteNames;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PreferenceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,22 @@ Route::post('/register', [AuthController::class, 'register'])->name(RouteNames::
 Route::post('/login', [AuthController::class, 'login'])->name(RouteNames::AUTH_LOGIN);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name(RouteNames::AUTH_LOGOUT);
 Route::post('/password/reset', [AuthController::class, 'resetPassword'])->name(RouteNames::PASSWORD_RESET);
+
+/*
+|--------------------------------------------------------------------------
+| Article Management Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| User Preferences Routes
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/preferences', [PreferenceController::class, 'show'])->name(RouteNames::PREF_SHOW);
+    Route::post('/preferences', [PreferenceController::class, 'store'])->name(RouteNames::PREF_STORE);
+});
