b6e7887f5fd70975ca6250f8b796c7003cc13889
Abdul Wadood <awadoodraj@gmail.com>
2024-10-23 17:02:57 +0200

    redirect to api documentation added to home page.

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/docs', function () {
-    return redirect('/swagger-ui');
+    return redirect('/api/documentation');
 });
