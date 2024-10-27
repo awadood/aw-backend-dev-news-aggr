3e993ab69d526d130183a5072cfa3d4e65d7f066
Abdul Wadood <awadoodraj@gmail.com>
2024-10-27 17:45:43 +0100

    command name updated in the console.php

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('pull:articles')->cron(env('EXPR_PULL_ARTICLES', '0 * * * *'));
+Schedule::command('articles:fetch-and-store')->cron(env('EXPR_PULL_ARTICLES', '0 * * * *'));
